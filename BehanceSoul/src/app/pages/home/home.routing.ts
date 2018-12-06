3f0891007e572c582265ce1f5c6e075c89984898
Lucas Medina dos Santos <Lucas.santos@redspark.local>
2018-12-06 14:15:25 -0200

    Initial commit, basic specs and definitions

---
+++ BehanceSoul/src/app/pages/home/home.routing.ts
@@ -0,0 +1,20 @@
+import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [
+    HomeComponent
+  ],
+  providers: []
+})
+export class HomeRoutingModule { }

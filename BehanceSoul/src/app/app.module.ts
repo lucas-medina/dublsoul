bed41158a7979d4bdc8efad404eddc581b2034e4
Lucas Medina dos Santos <Lucas.santos@redspark.local>
2018-12-06 14:19:53 -0200

    Modifying router settings for lazyloaded modules

--- BehanceSoul/src/app/app.module.ts
+++ BehanceSoul/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,6 +14,7 @@ import { HomeModule } from './pages/home/home.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HomeModule
   ],
--- BehanceSoul/src/app/pages/home/home.module.ts
+++ BehanceSoul/src/app/pages/home/home.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './../../components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
